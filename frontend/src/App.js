974296a6bfb09988b09f97970e10fb3076240351
Christopher Shockley <cishockleysr@gmail.com>
2020-04-17 22:52:04 -0400

    signin form running with basic functionality

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,12 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import SignIn from "./components/SignIn";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-      testing HELLO WORLD....
+    <div className='App'>
+      <header className='App-header'>
+        <SignIn />
       </header>
     </div>
   );
