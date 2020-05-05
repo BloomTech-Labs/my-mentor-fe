3f7f4322094cb818e58f35116d5e5f42a002c74e
Hector Valdez <hvaldez180@gmail.com>
2020-05-05 00:34:09 -0400

    added home component

--- frontend/src/home-components/Home.js
+++ frontend/src/home-components/Home.js
@@ -1,9 +1,36 @@
 import React from "react";
+import styled from "styled-components";
+
+const BoxLeft = styled.div`
+font-size: 25px;
+background-color: #052354;
+width: 50%;
+height: 750px;
+`
+const H1 = styled.h1`
+color: white;
+`
+const P = styled.p`
+color: white;
+`
+const BoxRight = styled.div`
+
+`
+
 
 export default function Home() {
     return (
         <div>
-            
+            <BoxLeft>
+                <H1>Connect to your future, rewrite the past</H1>
+                <P>Search for mentors</P>
+                <P>Sponser a mentee</P>
+                <P>Customize your search</P>
+                <P>Be apart of a community</P>
+            </BoxLeft>
+            <BoxRight>
+                <img src="frontend/src/photos/joshua-ness--bEZ_OfWu3Y-unsplash.jpg" alt="Mentor and Mentee drinking coffee"/>
+            </BoxRight>
         </div>
     )
 }
