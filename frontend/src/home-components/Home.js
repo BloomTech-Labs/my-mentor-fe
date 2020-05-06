d855d22a4d33b74206d78114792c743c6b04e142
Hector Valdez <hvaldez180@gmail.com>
2020-05-05 23:30:02 -0400

    added home page looking for second opinion

--- frontend/src/home-components/Home.js
+++ frontend/src/home-components/Home.js
@@ -1,36 +1,20 @@
 import React from "react";
 import styled from "styled-components";
-
-const BoxLeft = styled.div`
-font-size: 25px;
-background-color: #052354;
-width: 50%;
-height: 750px;
-`
-const H1 = styled.h1`
-color: white;
-`
-const P = styled.p`
-color: white;
-`
-const BoxRight = styled.div`
-
-`
+import "./Home.css"
 
 
 export default function Home() {
     return (
         <div>
-            <BoxLeft>
-                <H1>Connect to your future, rewrite the past</H1>
-                <P>Search for mentors</P>
-                <P>Sponser a mentee</P>
-                <P>Customize your search</P>
-                <P>Be apart of a community</P>
-            </BoxLeft>
-            <BoxRight>
-                <img src="frontend/src/photos/joshua-ness--bEZ_OfWu3Y-unsplash.jpg" alt="Mentor and Mentee drinking coffee"/>
-            </BoxRight>
+            <div className="home-div">
+                <h1 className="home-header">Connect to your future, rewrite the past</h1>
+                <p>Search for mentors</p>
+                <p>Sponser a mentee</p>
+                <p>Customize your search</p>
+                <p>Be apart of a community</p>
+                 <img className="home-pic" src={require('./photos/joshua-unsplash.jpg')} alt="Mentor and Mentee drinking coffee"/>
+            </div>
+               
         </div>
     )
 }
