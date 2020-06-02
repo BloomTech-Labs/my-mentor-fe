4812927125a7872a76c001b6da0dc97ab1ad94e7
Miley Wright <dishaliekwright@yahoo.com>
2020-06-02 14:53:14 -0400

    mentee data is showing

--- src/components/dashboard/DashboardMentee.js
+++ src/components/dashboard/DashboardMentee.js
@@ -1,15 +1,28 @@
-import React from "react";
-import { connect } from 'react-redux';
+import React, {useState, useEffect} from "react";
+import { AxiosWithAuth } from '../../middleware/axioswithauth';
 import Nav from '../../home-components/nav-drawer';
 import UserInfoMentee from './userInfoMentee.js';
 
 
 function DashboardMentee(props) {
-
+    const [userLoggedIn, setUserLoggedIn] = useState([]);
+    const userStorage = useState(localStorage.getItem('email'));
+  
+    useEffect(() => {
+      AxiosWithAuth()
+      .get('https://mentor-be.herokuapp.com/api/mentee',{headers: {Authorization: localStorage.getItem('token')}})
+      .then(res => {
+        const currentUser = res.data.filter(user => (
+          user.email === userStorage[0])
+        )[0];
+        setUserLoggedIn(currentUser);
+      })
+      .catch(err => console.log(err.response))
+    }, []);
     return (
         <div>  
             <Nav />
-            <UserInfoMentee />
+            <UserInfoMentee currentUser={userLoggedIn} />
         </div>
     )
 };
