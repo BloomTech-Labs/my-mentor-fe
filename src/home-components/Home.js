c40aaf04f242d590a216a6c7f178ea9262cad317
Miley Wright <dishaliekwright@yahoo.com>
2020-06-02 18:39:48 -0400

    typewriter effect

--- src/home-components/Home.js
+++ src/home-components/Home.js
@@ -5,6 +5,62 @@ import { Link } from "react-router-dom";
 import Button from "../mentorProfile/src/components/CustomButtons/Button";
 
 export default function Home() {
+  var TxtType = function(el, toRotate, period) {
+    this.toRotate = toRotate;
+    this.el = el;
+    this.loopNum = 0;
+    this.period = parseInt(period, 10) || 2000;
+    this.txt = '';
+    this.tick();
+    this.isDeleting = false;
+};
+
+TxtType.prototype.tick = function() {
+    var i = this.loopNum % this.toRotate.length;
+    var fullTxt = this.toRotate[i];
+
+    if (this.isDeleting) {
+    this.txt = fullTxt.substring(0, this.txt.length - 1);
+    } else {
+    this.txt = fullTxt.substring(0, this.txt.length + 1);
+    }
+
+    this.el.innerHTML = '<span class="wrap">'+this.txt+'</span>';
+
+    var that = this;
+    var delta = 200 - Math.random() * 100;
+
+    if (this.isDeleting) { delta /= 2; }
+
+    if (!this.isDeleting && this.txt === fullTxt) {
+    delta = this.period;
+    this.isDeleting = true;
+    } else if (this.isDeleting && this.txt === '') {
+    this.isDeleting = false;
+    this.loopNum++;
+    delta = 500;
+    }
+
+    setTimeout(function() {
+    that.tick();
+    }, delta);
+};
+
+window.onload = function() {
+    var elements = document.getElementsByClassName('typewrite');
+    for (var i=0; i<elements.length; i++) {
+        var toRotate = elements[i].getAttribute('data-type');
+        var period = elements[i].getAttribute('data-period');
+        if (toRotate) {
+          new TxtType(elements[i], JSON.parse(toRotate), period);
+        }
+    }
+    // INJECT CSS
+    var css = document.createElement("style");
+    css.type = "text/css";
+    css.innerHTML = ".typewrite > .wrap { border-right: 0.08em solid #fff}";
+    document.body.appendChild(css);
+};
 
   return (
     <div>
@@ -14,15 +70,14 @@ export default function Home() {
       </Button>
       <Button simple>
         <Link to="/menteeLogin">Mentee Login</Link>
-      </Button>
-      <div className="home-div">
-        <h1 className="home-header">
-          Connect to your future, rewrite the past
+      </Button>   
+       <h1 className="home-header">
+          {/* Connect to your future, rewrite the past */}
+          <a href="/" className="typewrite" data-period="2000" data-type='["Search for mentors.", "Sponser a mentee.", "Customize your search.", "Be apart of a community."]'>
+            <span className='wrap'></span>
+          </a>
         </h1>
-        <p>Search for mentors</p>
-        <p>Sponser a mentee</p>
-        <p>Customize your search</p>
-        <p>Be apart of a community</p>
+      <div className="home-div">
         <img
           className="home-pic"
           src={require("./photos/joshua-unsplash.jpg")}
