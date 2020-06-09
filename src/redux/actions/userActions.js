bcdc6ea9a026db9bfbfd91d0e612aa27f5f46d13
Hector Valdez <hvaldez180@gmail.com>
2020-06-08 22:30:47 -0400

    added post and update to reducer

--- src/redux/actions/userActions.js
+++ src/redux/actions/userActions.js
@@ -48,6 +48,27 @@ export const addMenteePost = menteePost => dispatch => {
     });
 }
 
+export const BEGIN_MENTEE_UPDATE_POST = 'BEGIN_MENTEE_UPDATE_POST';
+export const UPDATE_MENTEE_POST_SUCCESS = 'UPDATE_MENTEE_POST_SUCCESS';
+export const UPDATE_MENTEE_POST_FAILURE = 'UPDATE_MENTEE_POST_FAILURE';
+
+export const updatePost = (id, post) => dispatch => {
+  dispatch({ type: BEGIN_MENTEE_UPDATE_POST });
+
+  axiosWithAuth()
+  .put("API GOES HERE", {
+    
+  })
+  .then(res => {
+    dispatch({ type: UPDATE_MENTEE_POST_SUCCESS, payload: res.data, message: 'successfully updated joke'})
+  })
+  .catch(error => {
+    console.error(error);
+
+    dispatch({ type: UPDATE_MENTEE_POST_FAILURE , payload: error.response.data.message })
+  });
+}
+
 export const BEGIN_MENTEE_PUT_PROFILE = 'BEGIN_ADD_MENTEE_POST';
 export const MENTEE_PUT_PROFILE_SUCCESS = 'ADD_MENTEE_POST_SUCCESS';
 export const MENTEE_PUT_PROFILE_FAILURE = 'ADD_MENTEE_POST_FAILURE';
--- src/redux/reducer/reducer.js
+++ src/redux/reducer/reducer.js
@@ -1,37 +1,90 @@
 import {
+  BEGIN_ADD_MENTEE_POST,
+  ADD_MENTEE_POST_SUCCESS,
+  ADD_MENTEE_POST_FAILURE,
+  BEGIN_MENTEE_UPDATE_POST,
+  UPDATE_MENTEE_POST_SUCCESS,
+  UPDATE_MENTEE_POST_FAILURE,
 } from "../actions";
 
 const initialState = {
-    email: "",
-    isAuthenticating: false,
-    loggedIn: false,
-    authenticationError: "",
-}
+  email: "",
+  isAuthenticating: false,
+  loggedIn: false,
+  authenticationError: "",
 
-export const reducer =  (state = initialState, action) => {
-    switch (action.type) {
-        case START_MENTOR_LOGIN:
-            return {
-                ...state,
-                isAuthenticating: true
-            }
-        case LOGIN_MENTOR_SUCCESS:
-            return {
-                ...state,
-                isAuthenticating: false,
-                loggedIn: true,
-                email: email.email
-            }
-        case LOGIN_MENTOR_FAILURE:
-            return {
-                ...state,
-                authenticationError: action.payload,
-                isAuthenticating:false
-            }
-        
-        default:
-            return {
-                ...state
+  posts: [],
+
+  isFetchingPost: false,
+  postsError: "",
+  postsSuccess: "",
+};
+
+export const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    //first three case switches need review - hector
+    case START_MENTEE_LOGIN:
+      return {
+        ...state,
+        isAuthenticating: true,
+      };
+    case LOGIN_MENTEE_SUCCESS:
+      return {
+        ...state,
+        isAuthenticating: false,
+        loggedIn: true,
+        email: email.email,
+      };
+    case LOGIN_MENTEE_FAILURE:
+      return {
+        ...state,
+        authenticationError: action.payload,
+        isAuthenticating: false,
+      };
+    case BEGIN_ADD_MENTEE_POST:
+      return {
+        ...state,
+        isFetchingPost: true,
+      };
+    case ADD_MENTEE_POST_SUCCESS:
+      return {
+        ...state,
+        isFetchingPost: false,
+        postsSuccess: "Post was added.",
+        posts: [...state.posts, action.payload],
+      };
+    case ADD_MENTEE_POST_FAILURE:
+      return {
+        ...state,
+        isFetchingPost: false,
+        postsError: action.payload,
+      };
+    case BEGIN_MENTEE_UPDATE_POST:
+      return {
+        ...state,
+        isFetchingPost: true,
+      };
+    case UPDATE_MENTEE_POST_SUCCESS:
+      return {
+        ...state,
+        isFetchingPost: false,
+        posts: state.posts.map((post) => {
+          if (post.id === action.payload.id) {
+            return action.payload;
+          } else {
+            return post;
+          }
+        }),
+      };
+    case UPDATE_MENTEE_POST_SUCCESS:
+        return {
+            ...state,
+            isFetchingPost: false
         }
-    }
+
+    default:
+      return {
+        ...state,
+      };
+  }
 };
